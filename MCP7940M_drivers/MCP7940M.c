f5d67eb69da70a2f363c4a9264f367c4144c4e73
emigomezg <47527289+emigomezg@users.noreply.github.com>
2019-11-17 17:33:46 -0600

    Added day, month and year, gets and sets

--- MCP7940M_drivers/MCP7940M.c
+++ MCP7940M_drivers/MCP7940M.c
@@ -12,6 +12,10 @@ static const uint8_t seconds_memory = 0x00;
 static const uint8_t minutes_memory = 0x01;
 static const uint8_t hours_memory = 0x02;
 
+static const uint8_t day_memory = 0x04;
+static const uint8_t month_memory = 0x05;
+static const uint8_t year_memory = 0x06;
+
 void MCP7940M_set_seconds(uint8_t seconds)
 {
 	if(seconds>=0 &&  seconds<60){
@@ -75,6 +79,7 @@ void MCP7940M_set_hours(uint8_t hours)
 		I2C_stop(I2C_0);//sends nack and stop comunications
 	}
 }
+
 uint8_t MCP7940M_get_seconds(void)
 {
 	uint8_t data = 0x00;
@@ -159,4 +164,228 @@ uint8_t MCP7940M_get_hours(void)
 	data = I2C_read_byte(I2C_0);//read real value
 	(data&= ~(0xC0));
 	 return  ((data & 0xF0) >> 4) * 10 + (data & 0x0F);
+}
+
+void MCP7940M_set_day(uint8_t day, uint8_t month, uint8_t year)
+{
+	uint8_t day_limit = 0;
+	/*Month of input date is validated*/
+	if(month>=1 &&  month<13)
+	{
+		/*Depending on the month the limit day is validated*/
+		switch (month)
+		{
+			case JAN:
+			{
+				day_limit = 31;
+				break;
+			}
+			case FEB:
+			{
+				if(((year % 4 == 0) && (year % 100!= 0)) || (year % 400 == 0))
+				{
+					day_limit = 29;
+				}
+				else
+					day_limit = 28;
+			}
+			case MAR:
+			{
+				day_limit = 31;
+				break;
+			}
+			case APR:
+			{
+				day_limit = 30;
+				break;
+			}
+			case MAY:
+			{
+				day_limit = 31;
+				break;
+			}
+			case JUN:
+			{
+				day_limit = 30;
+				break;
+			}
+			case JUL:
+			{
+				day_limit = 31;
+				break;
+			}
+			case AUG:
+			{
+				day_limit = 31;
+				break;
+			}
+			case SEP:
+			{
+				day_limit = 30;
+				break;
+			}
+			case OCT:
+			{
+				day_limit = 31;
+				break;
+			}
+			case NOV:
+			{
+				day_limit = 30;
+				break;
+			}
+			case DEC:
+			{
+				day_limit = 31;
+				break;
+			}
+		}
+		if(day>=1 &&  day<day_limit)
+			{
+				day =((day / 10) << 4) | (day % 10);
+				I2C_start(I2C_0);//sends start bit
+				I2C_write_byte(I2C_0, MCP7940M_WRITE_ADD);//writes the direction of RTC with the write bit enable
+				I2C_wait(I2C_0);//waits for response in the line
+				I2C_get_ack(I2C_0);//reads the acknowledge
+
+				I2C_write_byte(I2C_0, day_memory);//the memory direction for seconds
+				I2C_wait(I2C_0);//waits for acknowledge
+				I2C_get_ack(I2C_0);//reads acknowledge
+
+				I2C_write_byte(I2C_0, day);//set seconds and start the clock
+				I2C_wait(I2C_0);//wait for acknowledge
+				I2C_get_ack(I2C_0);//reads acknowledge
+
+				I2C_stop(I2C_0);//sends nack and stop comunications
+			}
+	}
+}
+
+void MCP7940M_set_month(uint8_t month)
+{
+	if(month>=1 &&  month<13)
+	{
+		month =((month / 10) << 4) | (month % 10);
+		I2C_start(I2C_0);//sends start bit
+		I2C_write_byte(I2C_0, MCP7940M_WRITE_ADD);//writes the direction of RTC with the write bit enable
+		I2C_wait(I2C_0);//waits for response in the line
+		I2C_get_ack(I2C_0);//reads the acknowledge
+
+		I2C_write_byte(I2C_0, month_memory);//the memory direction for seconds
+		I2C_wait(I2C_0);//waits for acknowledge
+		I2C_get_ack(I2C_0);//reads acknowledge
+
+		I2C_write_byte(I2C_0, month);//set seconds and start the clock
+		I2C_wait(I2C_0);//wait for acknowledge
+		I2C_get_ack(I2C_0);//reads acknowledge
+
+		I2C_stop(I2C_0);//sends nack and stop comunications
+	}
+}
+
+void MCP7940M_set_year(uint8_t year)
+{
+	if(year>=1 &&  year<13)
+	{
+		year =((year / 10) << 4) | (year % 10);
+		I2C_start(I2C_0);//sends start bit
+		I2C_write_byte(I2C_0, MCP7940M_WRITE_ADD);//writes the direction of RTC with the write bit enable
+		I2C_wait(I2C_0);//waits for response in the line
+		I2C_get_ack(I2C_0);//reads the acknowledge
+
+		I2C_write_byte(I2C_0, year_memory);//the memory direction for seconds
+		I2C_wait(I2C_0);//waits for acknowledge
+		I2C_get_ack(I2C_0);//reads acknowledge
+
+		I2C_write_byte(I2C_0, year);//set seconds and start the clock
+		I2C_wait(I2C_0);//wait for acknowledge
+		I2C_get_ack(I2C_0);//reads acknowledge
+
+		I2C_stop(I2C_0);//sends nack and stop comunications
+	}
+}
+
+uint8_t MCP7940M_get_day(void)
+{
+	uint8_t data = 0x00;
+	I2C_start(I2C_0);//sends start bit
+	I2C_write_byte(I2C_0, MCP7940M_WRITE_ADD);//sends RTC address + write bit
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//reads acknowledge
+
+	I2C_write_byte(I2C_0, day_memory);//states you want to read the day
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//reads  acknowledge
+
+	I2C_repeted_start(I2C_0);// sends re-start bit
+	I2C_write_byte(I2C_0, MCP7940M_READ_ADD);//sends RTC address + read bit
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//read acknowledge
+
+	I2C_tx_rx_mode(I2C_0, I2C_RECIVER);//change to receiver mode
+
+	I2C_nack(I2C_0);//send NACK signal
+	I2C_read_byte(I2C_0);//reads dummy value
+	I2C_wait(I2C_0);//wait to line clear
+
+	I2C_stop(I2C_0);//sends stop bit
+	data = I2C_read_byte(I2C_0);//read real value
+	return  ((data & 0xF0) >> 4) * 10 + (data & 0x0F);
+}
+
+uint8_t MCP7940M_get_month(void)
+{
+	uint8_t data = 0x00;
+	I2C_start(I2C_0);//sends start bit
+	I2C_write_byte(I2C_0, MCP7940M_WRITE_ADD);//sends RTC address + write bit
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//reads acknowledge
+
+	I2C_write_byte(I2C_0, month_memory);//states you want to read the month
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//reads  acknowledge
+
+	I2C_repeted_start(I2C_0);// sends re-start bit
+	I2C_write_byte(I2C_0, MCP7940M_READ_ADD);//sends RTC address + read bit
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//read acknowledge
+
+	I2C_tx_rx_mode(I2C_0, I2C_RECIVER);//change to receiver mode
+
+	I2C_nack(I2C_0);//send NACK signal
+	I2C_read_byte(I2C_0);//reads dummy value
+	I2C_wait(I2C_0);//wait to line clear
+
+	I2C_stop(I2C_0);//sends stop bit
+	data = I2C_read_byte(I2C_0);//read real value
+	(data&= ~(0x20));
+	return  ((data & 0xF0) >> 4) * 10 + (data & 0x0F);
+}
+
+uint8_t MCP7940M_get_year(void)
+{
+	uint8_t data = 0x00;
+	I2C_start(I2C_0);//sends start bit
+	I2C_write_byte(I2C_0, MCP7940M_WRITE_ADD);//sends RTC address + write bit
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//reads acknowledge
+
+	I2C_write_byte(I2C_0, year_memory);//states you want to read the year
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//reads  acknowledge
+
+	I2C_repeted_start(I2C_0);// sends re-start bit
+	I2C_write_byte(I2C_0, MCP7940M_READ_ADD);//sends RTC address + read bit
+	I2C_wait(I2C_0);//waits for response
+	I2C_get_ack(I2C_0);//read acknowledge
+
+	I2C_tx_rx_mode(I2C_0, I2C_RECIVER);//change to receiver mode
+
+	I2C_nack(I2C_0);//send NACK signal
+	I2C_read_byte(I2C_0);//reads dummy value
+	I2C_wait(I2C_0);//wait to line clear
+
+	I2C_stop(I2C_0);//sends stop bit
+	data = I2C_read_byte(I2C_0);//read real value
+	return  ((data & 0xF0) >> 4) * 10 + (data & 0x0F);
 }
